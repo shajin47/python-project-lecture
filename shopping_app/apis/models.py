31ed0a0abe8649e88d29237bacb9785a03518c16
Shajin <shajinrj145@gmail.com>
2023-11-23 15:34:31 +0530

    created product table

--- shopping_app/apis/models.py
+++ shopping_app/apis/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Products(models.Model):
+    Product_Name = models.CharField(max_length=100,blank=False)
+    HSN_Code = models.IntegerField(blank=False, unique=True)
+    Cost_Price = models.DecimalField(decimal_places=2, max_digits=10)
+    Selling_Price = models.DecimalField(decimal_places=2, max_digits=10)
+
+    def __str__(self):
+        return "%s" % (self.Product_Name)  
--- shopping_app/apis/views.py
+++ shopping_app/apis/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Products
+from .serializers import productSerializer
 # Create your views here.
+
+@api_view(['POST'])
+def createProducts(request):
+    try:
+        data = request.data
+        product = Products.objects.create(
+            Product_Name = data['Product_Name'],
+            HSN_Code = data['HSN_Code'],
+            Cost_Price = data['Cost_Price'],
+            Selling_Price = data['Selling_Price']
+        )
+        instance = productSerializer(product)
+        return Response(instance.data)
+    except:
+        return Response({"success": False, "message": "Something went wrong might be duplicate record"})
+
+ 
+
+
